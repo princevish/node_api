d678179fc3d75a920fa60aba5784b08a901c600c
unknown <princevworld@gmail.com>
2021-07-09 23:53:12 +0530

    complete

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-require('dotenv').config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const port = 4000;
-const db = require('./config/config');
-const passport_jwt = require('./config/passort_jwt');
-const cookieParser = require('cookie-parser');
+const port = process.env.PORT;
+const db = require("./config/config");
+const passport_jwt = require("./config/passort_jwt");
+const cookieParser = require("cookie-parser");
+const compression = require("compression");
 
-
-app.use(express.urlencoded({
-    extended: true
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-// multer errors
-app.use(function(err,req,res,next){
-    console.log("errr");
-    res.status(500).json({err:err});
-})
+app.use(compression({ level: 6 }));
 
-app.use('/upload', express.static('upload'));
+// multer errors
+app.use(function (err, req, res, next) {
+  res.status(500).json({ err: err });
+});
+
+app.use("/upload", express.static("upload"));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
- res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
   next();
 });
-app.use('/api', require('./routes'));
-app.use((req,res,next)=>{
-    res.status(404).json({error:'url not found'});
-})
+app.use("/api", require("./routes"));
+app.use((req, res, next) => {
+  res.status(404).json({ error: "url not found" });
+});
 
-app.listen(port, () => console.log(`Example app listening on port port! ${port}`));
+app.listen(port, () =>
+  console.log(`Example app listening on port port! ${port}`)
+);
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,17 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
-const auth =require('../config/auth')
+const auth = require("../config/auth");
 //passport auth for header token verification
 //const passport = require('passport');
 //passport.authenticate('jwt', { session: false})
 
-router.use('/room', require('./roomRoutes'));
+router.use("/room", require("./roomRoutes"));
 
-router.get('/auth',auth,async(req,res,next)=>{
-    
-	 return res.status(200).json({
-                    id: req.userID
-                });
+router.get("/auth", auth, async (req, res, next) => {
+  return res.status(200).json({
+    id: req.userID,
+  });
 });
 
-router.use('/users', require('./usersRoutes'));
+router.use("/users", require("./usersRoutes"));
 module.exports = router;
